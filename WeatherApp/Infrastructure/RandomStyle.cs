36b1bc5c1aaf98b2a4b61fe086c3d9c995ac4b49
Maxim Vitugov <mvitugov@gmail.com>
2024-07-02 17:23:39 +0200

    +layout

---
+++ WeatherApp/Infrastructure/RandomStyle.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherApp.Infrastructure
+{
+    public class RandomStyle
+    {
+        private List<string> styleList { get; } = [ "widget-49-date-primary", "widget-49-date-success", "widget-49-date-info",
+        "widget-49-date-warning", "widget-49-date-danger", "widget-49-date-dark", "widget-49-date-base"];
+        private Random Random { get; } = new();
+        public string Get() => styleList[GetRandom()];
+        private int GetRandom() => Random.Next(styleList.Count);
+    }
+}
