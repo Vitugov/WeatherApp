8f5480314ce6f40d303f89f92ff230eaa71d9282
Maxim Vitugov <mvitugov@gmail.com>
2024-07-03 17:49:53 +0200

    ViewComponent update

---
+++ WeatherApp/ViewComponents/CityWeatherCardViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewComponents
+{
+    public class CityWeatherCardViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather cityWeather, bool isLinkToHome = false) 
+        { 
+            ViewBag.IsLinkToHome = isLinkToHome;
+            return View(cityWeather);
+        }
+    }
+}
